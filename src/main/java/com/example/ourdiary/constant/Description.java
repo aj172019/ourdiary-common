378c4bdf71bf221bc9baa54dfa18c04e9727bb36
안준헌 <frozengrape@mz.co.kr>
2023-08-18 13:15:13 +0900

    upgrade spring boot to 3.2.0-M1

--- src/main/java/com/example/ourdiary/constant/Description.java
+++ src/main/java/com/example/ourdiary/constant/Description.java
@@ -10,7 +10,7 @@ public class Description {
     public static final String API_AUTH_LOGIN = """
                             `Access Token`, `Refresh Token`을 발행합니다.<br/>
                             `Access Token`은 `Header`에<br/>
-                            `Refresh Token`은 `Cookie`의 `refresh-token`으로 전달됩니다zzz.
+                            `Refresh Token`은 `Cookie`의 `refresh-token`으로 전달됩니다.
                             """;
 
     public static final String API_AUTH_LOGOUT = """
